e9431c338dfc034dbe8cbf05914ed13918bf8750
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-03-08 11:20:57 +0100

    created AuthResponse

--- backend/src/main/java/com/de/boederij/controller/AuthController.java
+++ backend/src/main/java/com/de/boederij/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.de.boederij.config.exception.BadRequestException;
 import com.de.boederij.model.AuthProvider;
 import com.de.boederij.model.User;
 import com.de.boederij.payload.ApiResponse;
+import com.de.boederij.payload.AuthResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
