f60954e6a5d5f77f8d39f122fe029734270a870d
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-03-08 09:28:15 +0100

    added new folders to repo with an initialized spring backend and react frontend

---
+++ backend/src/main/java/com/de/boederij/BoederijApplication.java
@@ -0,0 +1,13 @@
+package com.de.boederij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoederijApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoederijApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/de/boederij/BoederijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.de.boederij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoederijApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
