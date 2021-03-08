823ae925a8967b227f5f666f41e1e1a0c3f6b583
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-03-08 11:07:03 +0100

    added two new exceptions: OAuth2AuthenticationProcessingException and ResourceNotFoundException

--- backend/src/main/java/com/de/boederij/controller/UserController.java
+++ backend/src/main/java/com/de/boederij/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.de.boederij.controller;
 
+import com.de.boederij.config.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
