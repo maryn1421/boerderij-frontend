b63086da13cef01fe307d158f43d78d1ae1d3910
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-03-08 11:29:13 +0100

    create a new directory: security and added an oauth2 folder and a current user class into it

--- backend/src/main/java/com/de/boederij/controller/UserController.java
+++ backend/src/main/java/com/de/boederij/controller/UserController.java
@@ -3,6 +3,7 @@ package com.de.boederij.controller;
 import com.de.boederij.config.exception.ResourceNotFoundException;
 import com.de.boederij.model.User;
 import com.de.boederij.repository.UserRepository;
+import com.de.boederij.security.CurrentUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
