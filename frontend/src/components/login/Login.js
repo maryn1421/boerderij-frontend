fa84b6c8d6ff6e4a248424ece08de8d0234287c5
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-03-08 15:48:05 +0100

    added background image to login page

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -2,15 +2,15 @@ import React from "react";
 import SocialLogin from "./SocialLogin";
 import LoginForm from "./LoginForm";
 import './login.css'
+import {Link} from "react-router-dom";
 
 const Login = () => {
+    document.body.style.backgroundImage = "url('/images/farm-background.jpg')";
     return <div className="login-container">
             <div className="login-content">
                 <h1 className="login-title">Belépés</h1>
-                <div className="or-separator">
-                </div>
                 <LoginForm />
-                <span className="signup-link">New user?</span>
+                <span className="signup-link">Új vagy? <Link to={"/signup"}>Regisztráció</Link></span>
             </div>
     </div>
 }
