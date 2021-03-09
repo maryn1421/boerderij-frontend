bc9dd611c5a694d526c4f4ec71982d8a1a5e22fa
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-03-09 15:22:43 +0100

    added new farm background image and new component farmMenu

---
+++ frontend/src/pages/farmPage/FarmMenu.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const FarmMenu = () => {
+    return   <div className="farmPage__menuContainer">
+        <div className="farmPage__menu">
+            <div className="farmPage__menuItem">
+                <p>Kiadások</p>
+            </div>
+            <div className="farmPage__menuItem">
+                <p>Bevételek</p>
+            </div>
+            <div className="farmPage__menuItem">
+                <p>Állatok</p>
+            </div>
+            <div className="farmPage__menuItem">
+                <p>Rendelések</p>
+            </div>
+            <div className="farmPage__menuItem">
+                <p>Piac</p>
+            </div>
+        </div>
+    </div>
+}
+export default FarmMenu
--- frontend/src/pages/farmPage/FarmPage.js
+++ frontend/src/pages/farmPage/FarmPage.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import './farmPage.css'
+import FarmMenu from "./FarmMenu";
 
 const FarmPage = () => {
+    document.body.style.backgroundImage = "url('/images/farm-image.jpg')";
+
+
     return <div className="farmPage__main">
-        
+        <FarmMenu/>
     </div>
 }
 
