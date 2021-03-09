849997f4ea6aed9ee02b35d2e36f3f593c9b80cd
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-03-09 15:00:10 +0100

    finished entity testing

--- backend/src/test/java/com/de/boederij/model/OrderTest.java
+++ backend/src/test/java/com/de/boederij/model/OrderTest.java
@@ -2,23 +2,37 @@ package com.de.boederij.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
+    Order testOrder = new Order();
 
     @Test
-    void getId() {
+    void getAndSetId() {
+        testOrder.setId(1L);
+        assertEquals(1L, testOrder.getId());
+
     }
 
     @Test
-    void getName() {
+    void getAndSetName() {
+        testOrder.setName("test");
+        assertEquals("test", testOrder.getName());
     }
 
     @Test
-    void getDate() {
+    void getAndSetDate() {
+        Date testDate = new Date();
+        testOrder.setDate(testDate);
+        assertEquals(testDate, testOrder.getDate());
     }
 
     @Test
-    void getUserId() {
+    void getAndSetUserId() {
+        testOrder.setUserId(7L);
+        assertEquals(7L, testOrder.getUserId());
     }
 }
