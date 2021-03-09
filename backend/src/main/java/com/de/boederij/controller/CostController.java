d7eabebfcd419df937e4b61b62eb6bf5dcbb3abc
hausbence <bence.hausknecht@gmail.com>
2021-03-09 15:38:32 +0100

    implemented CostController methods

--- backend/src/main/java/com/de/boederij/controller/CostController.java
+++ backend/src/main/java/com/de/boederij/controller/CostController.java
@@ -1,9 +1,17 @@
 package com.de.boederij.controller;
 
+import com.de.boederij.model.Animal;
+import com.de.boederij.model.Cost;
 import com.de.boederij.repository.CostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cost")
@@ -11,5 +19,35 @@ public class CostController {
 
     @Autowired
     CostRepository costRepository;
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/{user_id}/all")
+    public List<Cost> getAllCostsByUserId(@PathVariable("user_id") Long userId) {
+        return costRepository.getAllByUserId(userId);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/{user_id}/{cost_name}")
+    public List<Cost> getAllCostsByUserIdAndName(@PathVariable("user_id") Long userId, @PathVariable("cost_name") String name) {
+        return costRepository.getAllByUserIdAndName(userId, name);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/{user_id}/{date}")
+    public List<Cost> getAllCostsByUserIdAndDate(@PathVariable("user_id") Long userId, @PathVariable("date") Date date) {
+        return costRepository.getAllByUserIdAndDate(userId, date);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/{user_id}/{from}/{to}")
+    public List<Cost> getAllCostsBetweenDates(@PathVariable("user_id") Long userId, @PathVariable("from") Date from, @PathVariable("to") Date to) {
+        return costRepository.getAllByDateBetween(from, to);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/{user_id}/{cost_id}")
+    public List<Cost> getAllCostsBetweenDates(@PathVariable("user_id") Long userId, @PathVariable("cost_id") Long costId) {
+        return costRepository.getAllByUserIdAndId(userId, costId);
+    }
 
 }
--- backend/src/main/java/com/de/boederij/repository/CostRepository.java
+++ backend/src/main/java/com/de/boederij/repository/CostRepository.java
@@ -1,9 +1,23 @@
 package com.de.boederij.repository;
 
+import com.de.boederij.model.Animal;
 import com.de.boederij.model.Cost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface CostRepository extends JpaRepository<Cost, Long> {
+
+    List<Cost> getAllByUserId(Long userId);
+
+    List<Cost> getAllByUserIdAndDate(Long userId, Date date);
+
+    List<Cost> getAllByDateBetween(Date from, Date to);
+
+    List<Cost> getAllByUserIdAndName(Long userId, String name);
+
+    List<Cost> getAllByUserIdAndId(Long userId, Long costId);
 }
