c2b73748536973ada8faa6cc6a42a2e8a2cf583e
hausbence <bence.hausknecht@gmail.com>
2021-03-09 15:42:03 +0100

    implemented IncomeController methods

--- backend/src/main/java/com/de/boederij/repository/IncomeRepository.java
+++ backend/src/main/java/com/de/boederij/repository/IncomeRepository.java
@@ -1,9 +1,22 @@
 package com.de.boederij.repository;
 
+import com.de.boederij.model.Cost;
 import com.de.boederij.model.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface IncomeRepository extends JpaRepository<Income, Long> {
+    List<Income> getAllByUserId(Long userId);
+
+    List<Income> getAllByUserIdAndName(Long userId, String name);
+
+    List<Income> getAllByUserIdAndDate(Long userId, Date date);
+
+    List<Income> getAllByDateBetween(Date from, Date to);
+
+    List<Income> getAllByUserIdAndId(Long userId, Long costId);
 }
