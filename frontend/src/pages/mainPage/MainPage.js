e52d0bcb2b56101e16ec6030f99000376d1fcadb
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-03-11 13:08:04 +0100

    added a main page component

---
+++ frontend/src/pages/mainPage/MainPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const MainPage = () => {
+
+    document.body.style.backgroundColor = "black"
+
+
+    return <div className="mainPage">
+
+    </div>
+}
+
+
+export default MainPage;
