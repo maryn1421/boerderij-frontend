f19c398bacdae1e0f676fe3cab31227fad326ec3
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-03-11 11:24:18 +0100

    added new page component: cost page

---
+++ frontend/src/pages/farmPage/farmPageCost/FarmCostPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FarmMenu from "../FarmMenu";
+
+
+
+const FarmCostPAge = () => {
+    document.body.style.backgroundImage = "url('/images/farm-image.jpg')";
+
+    return <div className="farmPage__main">
+        <FarmMenu/>
+    </div>
+}
+
+export default FarmCostPAge;
