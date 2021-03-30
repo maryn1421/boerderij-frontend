18ea738326fdff4f2461e2746ec487826e676d2e
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-03-30 11:29:12 +0200

    created a new component: AllOrder.js

---
+++ src/pages/farmPage/farmPageOrder/AllOrder.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+
+
+const AllOrder = () => {
+    const [orders, setOrders] = useState();
+
+
+
+
+
+    let content =  <div className="loading"><h1>LOADING</h1></div>
+
+    return content;
+
+
+}
+
+
+export default AllOrder
