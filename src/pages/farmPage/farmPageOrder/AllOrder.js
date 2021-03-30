c31b45809e1dba97a2e87f884ad9b2b386144e4c
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-03-30 11:32:44 +0200

    added a new useeffect and function for all order fetching

--- src/pages/farmPage/farmPageOrder/AllOrder.js
+++ src/pages/farmPage/farmPageOrder/AllOrder.js
@@ -1,14 +1,32 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import {API_BASE_URL} from "../../../constants";
+import {useCookies} from "react-cookie";
+import authHeader from "../../../security/auth-header";
 
 
 const AllOrder = () => {
     const [orders, setOrders] = useState();
+    const [cookies, setCookies] = useCookies("user");
 
 
+    useEffect(() => {
+        fetchAllOrderByUser().then(data => {
+            setOrders(data)
+        })
+    }, [])
 
 
+    const fetchAllOrderByUser = async () => {
+        try {
+            const resp = await axios.get(API_BASE_URL + "/order/" + cookies.user.id + "/all", {headers: authHeader(cookies.user)})
+            return resp.data;
+        } catch (e) {
+        }
+    }
 
-    let content =  <div className="loading"><h1>LOADING</h1></div>
+
+    let content = <div className="loading"><h1>LOADING</h1></div>
 
     return content;
 
