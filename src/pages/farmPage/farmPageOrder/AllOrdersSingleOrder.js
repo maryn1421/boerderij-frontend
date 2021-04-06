cb79ff3ece938ee4a153c7735d5a17f52110cbbc
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-04-06 14:11:13 +0200

    added finishing order

--- src/pages/farmPage/farmPageOrder/AllOrdersSingleOrder.js
+++ src/pages/farmPage/farmPageOrder/AllOrdersSingleOrder.js
@@ -12,7 +12,7 @@ const AllOrdersSingleOrder = (props) => {
 
 
     const handleSetOrderPaid = () => {
-        if (window.confirm("Biztosan kifizetettre állítod a " + props?.data.name +  " nevü rendelést ")) {
+        if (window.confirm("Biztosan kifizetettre állítod a " + props?.data.name +  " nevű rendelést ")) {
             setOrderPaid().then(response => {
                 if (response !== null) {
                     new Alert("success", response).showAlert();
@@ -25,11 +25,33 @@ const AllOrdersSingleOrder = (props) => {
         }
     }
 
-    console.log(props.data)
+
+    const handleSetOrderFinished = () => {
+        if (window.confirm("biztosan lezártra állítod a " + props.data.name + "  nevű rendelést")) {
+            setOrderFinished().then(response => {
+                if (response !== null) {
+                    new Alert("success", response).showAlert();
+                }
+                else {
+                    new Alert("error", "hiba történt").showAlert();
+                }
+            })
+        }
+    }
 
     const setOrderPaid = async () => {
         try {
             const resp = await axios.put(API_BASE_URL + "/order/" + cookies.user.id + "/paid/"+ props.data.id, {}, {headers: authHeader(cookies.user)})
+            return resp.data
+        }
+        catch (e) {
+            new Alert("error", "hiba történt!").showAlert();
+        }
+    }
+
+    const setOrderFinished = async () => {
+        try {
+            const resp = await axios.put(API_BASE_URL + "/order/" + cookies.user.id + "/finish/"+ props.data.id, {}, {headers: authHeader(cookies.user)})
             return resp.data
         }
         catch (e) {
@@ -51,7 +73,7 @@ const AllOrdersSingleOrder = (props) => {
             classes += " finished__single"
         }
         if (props.data.isPaid) {
-            classes += " paid"
+            classes = "singleOrder__mainContainer paid"
         }
         return classes;
     }
@@ -62,11 +84,10 @@ const AllOrdersSingleOrder = (props) => {
         <p>{props?.data.name}</p>
         <p>{props?.data.price} forint</p>
         <div className="single__actionsContainer">
-            <p>{props?.data.finished ? " " : "Lezárás"}</p>
+            {props?.data.finished ? "" : <p onClick={e => handleSetOrderFinished()}>Lezárás</p>}
             {props?.data.isPaid ? "" : <p onClick={e => {handleSetOrderPaid()}}>Fizetve</p> }
             <p>Törlés</p>
         </div>
-
     </div>
 }
 
