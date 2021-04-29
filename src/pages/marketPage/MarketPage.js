340d7dc7f86bb4b0b8b60f67884028bf241c8379
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-04-29 10:16:45 +0200

    added a new folder: marketPage and 2 new components into it.

---
+++ src/pages/marketPage/MarketPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const MarketPage = () => {
+    return <div className="marketPage__main">
+        <h1>MARKET PAGE</h1>
+    </div>
+}
+
+
+export default MarketPage;
