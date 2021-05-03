488ee1c98e901eb9413c83cc484120f6654eea3a
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-05-03 14:48:12 +0200

    added a base design to the market page

--- src/pages/marketPage/MarketPage.js
+++ src/pages/marketPage/MarketPage.js
@@ -3,34 +3,41 @@ import axios from "axios";
 import {API_BASE_URL} from "../../constants";
 import MarketMenu from "./MarketMenu";
 import './market.css'
+import MarketFilter from "./MarketFilter";
+import SingleSale from "./SingleSale";
 
 
 const MarketPage = () => {
     const [sales, setSales] = useState([])
 
 
-
     useEffect(() => {
         fetchAllActiveSales().then(data => {
             setSales(data)
         })
-    },[])
+    }, [])
 
     console.log(sales)
 
     const fetchAllActiveSales = async () => {
         try {
-            const response = await  axios.get(API_BASE_URL + "/market/all-active")
+            const response = await axios.get(API_BASE_URL + "/market/all-active")
             return response.data
-        }
-        catch (e) {
+        } catch (e) {
             console.log(e)
         }
     }
 
     return <div className="marketPage__main">
         <h1>PIAC - DE BOERDERIJ</h1>
-        <MarketMenu />
+        <MarketMenu/>
+        <MarketFilter/>
+        <div className="sales__container">
+            {sales.map(sale => (
+                <SingleSale data={sale} key={sale.id}/>
+            ))}
+
+        </div>
     </div>
 }
 
--- src/pages/marketPage/SingleSale.js
+++ src/pages/marketPage/SingleSale.js
@@ -1,10 +1,30 @@
 import React from "react";
+import {API_BASE_URL} from "../../constants";
 
 
 const SingleSale = (props) => {
 
+    const formatDate = date => {
+        let newDate = new Date(date)
+        return newDate.toLocaleDateString();
+    }
+
+
 
     return <div className="singleSale__main">
+        <img className={"single_image"} src={API_BASE_URL + "/market/image/" + props?.data.photoName } alt=""/>
+        <div className="singleSale__description">
+            <p>{props.data.title}</p>
+            <p>{new Intl.NumberFormat().format(props.data.price)} Forint</p>
+
+            <div className="singleSale__bottomContainer">
+                <p>{props.data.province},</p>
+                <p>{props.data.city},</p>
+                <p>{formatDate(props.data.startDate)}</p>
+            </div>
+
+        </div>
+
     </div>
 }
 
