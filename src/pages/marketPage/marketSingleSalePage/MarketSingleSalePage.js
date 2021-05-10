9f29eea06de8e146b928bb25bcb07255dccbd591
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-05-10 10:42:59 +0200

    added single sale component

---
+++ src/pages/marketPage/marketSingleSalePage/MarketSingleSalePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './SingleSale.css'
+
+const MarketSingleSalePage = (props) => {
+
+    return <div className="marketSingleSalePage__main">
+
+    </div>
+}
+
+
+export default MarketSingleSalePage;
