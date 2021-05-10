cfb2be149ed1744c952584bfb56483c724bcad5e
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-05-10 15:00:44 +0200

    finished single sale pages map

--- src/pages/marketPage/marketSingleSalePage/MarketSingleSalePage.js
+++ src/pages/marketPage/marketSingleSalePage/MarketSingleSalePage.js
@@ -1,22 +1,95 @@
-import React, {useState} from "react";
-import { useParams } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 import './SingleSale.css'
+import {API_BASE_URL} from "../../../constants";
+import axios from "axios";
+import GoogleMapReact from 'google-map-react';
+
+
 
 const MarketSingleSalePage = (props) => {
     const [sale, setSale] = useState([]);
+    const [mapData, setMapData] = useState([])
+
+    let {id} = useParams();
+
+
+    useEffect(() => {
+        fetchSaleById().then(response => {
+            if (response.id === null) {
+                window.location.href = "/market/error"
+            } else {
+                setSale(response)
+                fetchMapData(response).then(resp => {
+                    setMapData(resp.results)
+                })
+            }
+        })
+
+    }, [])
 
 
 
-    const fetchSaleById = async ()  => {
 
+
+    console.log(mapData)
+
+    const fetchSaleById = async () => {
+        try {
+            const resp = await axios.get(API_BASE_URL + "/market/sale/" + id)
+            return resp.data
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const fetchMapData = async (sale) => {
+        console.log(sale.city)
+        try {
+            const resp = await axios.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + sale.city + "&region=hu&key=AIzaSyCxKALMMrebblFHpA3oZGeJoAA72lHHDkU")
+            return resp.data
+        } catch (e) {
+
+        }
+
+    }
+    let map = ""
+
+
+
+    if (mapData.length !== 0) {
+
+        const center = {
+            lat: mapData[0]?.geometry.location.lat,
+            lng: mapData[0]?.geometry.location.lng
+        }
+        console.log(center)
+
+
+        map =   <div >
+            <div className="saleMapContainer" style={{height: '40vh', width: '80%'}}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{key: "AIzaSyCxKALMMrebblFHpA3oZGeJoAA72lHHDkU"}}
+                    defaultCenter={center}
+                    defaultZoom={12}
+                >
+                </GoogleMapReact>
+            </div>
+        </div>
     }
 
 
-
-    let { id } = useParams();
-
     return <div className="marketSingleSalePage__main">
-        <h1>{id}</h1>
+        <div className="saleContainer">
+            <div>
+                <img className="saleImage" src={API_BASE_URL + "/market/image/" + sale?.photoName} alt=""/>
+            </div>
+            <div className="saleDataContainer">
+                <section>{sale?.description}</section>
+            </div>
+            {map}
+
+        </div>
     </div>
 }
 
