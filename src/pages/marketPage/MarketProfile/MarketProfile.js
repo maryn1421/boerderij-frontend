47a5598fc40f7d01d16ceb065de68871489426b3
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-05-14 12:13:38 +0200

    added an alert when a not logged in user wants to use private stuff

--- src/pages/marketPage/MarketProfile/MarketProfile.js
+++ src/pages/marketPage/MarketProfile/MarketProfile.js
@@ -1,19 +1,73 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useCookies} from "react-cookie";
+import './marketProfile.css'
+import axios from "axios";
+import {API_BASE_URL} from "../../../constants";
 
 const MarketProfile = () => {
     const [cookies, setCookies] = useCookies("user")
+    const [sales, setSales] = useState([]);
 
 
 
     document.body.style.backgroundColor = "#ADEFD1FF";
+    useEffect(() => {
+        if (cookies.user?.token) {
+            fetchSalesByUser().then(resp => {
+                setSales(resp)
+            })
+        }
+    }, [])
+
+
+    const formatDate = date => {
+        let newDate = new Date(date)
+        return newDate.toLocaleDateString();
+    }
 
 
 
-    if (cookies.user?.token) {
-        return <marketProfile className="main">
+    const fetchSalesByUser = async () => {
+        try {
+            const response = await axios.get(API_BASE_URL + "/market/all-sale/" + cookies.user.id)
+            return response.data
 
-        </marketProfile>
+        }
+        catch (e) {
+            console.log(e)
+        }
+
+    }
+
+
+    if (cookies.user?.token && sales) {
+
+        console.log(sales)
+
+
+        return <div className="marketProfile__main">
+            <div className="marketProfile__salesContainer">
+                {
+                    sales.map(sale => (
+                        <div className="singleSale__main">
+                            <img className={"single_image"} src={API_BASE_URL + "/market/image/" + sale?.photoName } alt=""/>
+                            <div className="singleSale__description">
+                                <p>{sale.title}</p>
+                                <p>{new Intl.NumberFormat().format(sale?.price)} Forint</p>
+
+                                <div className="singleSale__bottomContainer">
+                                    <p>{sale?.province},</p>
+                                    <p>{sale?.city},</p>
+                                    <p>{formatDate(sale?.startDate)}</p>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     }
     else {
         return <div className="private__main">
